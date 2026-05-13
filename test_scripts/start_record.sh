bcd73885f2f5bfe7f6bdf545a1c5af1abcef30ad
iankeefe <135243282+iankeefe@users.noreply.github.com>
2026-05-13 12:39:42 -0400

    Reliable trigger script?

--- test_scripts/start_record.sh
+++ test_scripts/start_record.sh
@@ -4,7 +4,8 @@
 set -e
 source /opt/ros/humble/setup.bash
 source "$(dirname "$(dirname "$(realpath "$0")")")/install/setup.bash"
-ros2 topic pub --once \
+ros2 topic pub --times 3 --rate 1 \
+    --wait-matching-subscriptions 1 \
     --qos-reliability reliable \
     --qos-durability transient_local \
     /camera/record_mode std_msgs/msg/String "{data: record}"
